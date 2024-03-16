f4bb1ea2012b227aecb770fc1896897085f581ab
Brandon Tran <tranbrandon1233@gmail.com>
2024-03-16 16:05:07 -0700

    Initial commit

---
+++ api.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+
+async function getData() {
+    try {
+        const response = await axios.get('https://datasets-server.huggingface.co/rows?dataset=jamescalam%2Fyoutube-transcriptions&config=default&split=train&offset=0&length=100');
+        const data = response.data;
+
+        // Create an object to store the word frequency
+        const wordFrequency = {};
+
+        // Loop through the text column and extract words
+        data.rows.forEach((row) => {
+            const text = row.row.text;
+            const words = text.split(/[,\. ]+/);
+
+            // Remove case sensitivity
+            words.forEach((word) => {
+                word = word.toLowerCase();
+
+                // Ignore words with less than 4 or more than 15 characters
+                if (word.length < 4 || word.length > 15) {
+                    return;
+                }
+
+                // Increment the frequency of the word
+                if (wordFrequency[word]) {
+                    wordFrequency[word]++;
+                } else {
+                    wordFrequency[word] = 1;
+                }
+            });
+        });
+
+        // Sort the word frequency object by value (frequency)
+        const sortedWordFrequency = Object.keys(wordFrequency).map((key) => {
+            return {
+                word: key,
+                frequency: wordFrequency[key]
+            };
+        }).sort((a, b) => b.frequency - a.frequency);
+
+        // Get the top 10 most frequent words
+        const topWords = sortedWordFrequency.slice(0, 10);
+
+        console.log(topWords);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getData();
