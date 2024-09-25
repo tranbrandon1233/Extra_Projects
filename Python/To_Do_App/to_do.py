45710f5744db025e00673a4981c36e4fdeffba65
tranbrandon1233 <70561420+tranbrandon1233@users.noreply.github.com>
2024-09-24 20:54:09 -0700

    Added files in Python and JS

--- Python/To_Do_App/to_do.py
+++ Python/To_Do_App/to_do.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, abort
 app = Flask(__name__)
 
 # Sample data for our to-do list
@@ -25,7 +25,7 @@ def get_todos(todo_id):
             if todo['id'] == todo_id:
                 return jsonify(todo)
         # Return error if todo not found
-        return jsonify({'message': 'Todo not found'}), 404
+        abort(404,description='Todo not found')
     else:
         # Return all todos
         return jsonify({'todos': todos})
@@ -33,55 +33,45 @@ def get_todos(todo_id):
 @app.route('/', methods=['POST'])
 def add_todo():
     # Handle POST request to add a new todo
-    try:
-        data = request.get_json()
-        # Error handling if received data is not valid
-        if not data or 'title' not in data:
-            return jsonify({"message": "Invalid data"}), 400
-        todo = {
-            'id': len(todos) + 1,
-            'title': data['title'],
-            'completed': False
-        }
-        # Append the new todo to the list
-        todos.append(todo)
-        return jsonify({'message': 'Todo added successfully', 'todo': todo}), 201
-    except Exception as e:
-        # Return error if something is wrong
-        return jsonify({'message': "An error occurred", 'error': str(e)}), 500
+    data = request.get_json()
+    # Error handling if received data is not valid
+    if not data or 'title' not in data:
+        abort(400, description="Invalid data")
+    todo = {
+        'id': len(todos) + 1,
+        'title': data['title'],
+        'completed': False
+    }
+    # Append the new todo to the list
+    todos.append(todo)
+    return jsonify({'message': 'Todo added successfully', 'todo': todo}), 201
+
 @app.route('/<int:todo_id>', methods=['PUT'])
 def update_todo(todo_id):
     # Handle PUT request to update existing todo
-    try:
-        data = request.get_json()
-        # Error handling to ensure data received is valid
-        if not data or 'title' not in data or 'completed' not in data:
-            return jsonify({'message': "Invalid data"}), 400
-        # Return todo based on ID
-        for todo in todos:
-            if todo['id'] == todo_id:
-                todo['title'] = data['title']
-                todo['completed'] = data['completed']
-                return jsonify({'message': 'Todo updated successfully', 'todo': todo})
-        return jsonify({'message': 'Todo not found'}), 404
-    except Exception as e:
-        # Return error if something is wrong
-        return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
+    data = request.get_json()
+    # Error handling to ensure data received is valid
+    if not data or 'title' not in data or 'completed' not in data:
+        return abort(400,description="Invalid data")
+    # Return todo based on ID
+    for todo in todos:
+        if todo['id'] == todo_id:
+            todo['title'] = data['title']
+            todo['completed'] = data['completed']
+            return jsonify({'message': 'Todo updated successfully', 'todo': todo})
+    abort(404,description='Todo not found')
+
 @app.route('/<int:todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
     # Handle DELETE request to delete a todo
     global todos
-    try:
-        # Delete the todo based on ID
-        for i, todo in enumerate(todos):
-            if todo['id'] == todo_id:
-                todos.pop(i)
-                return jsonify({'message': 'Todo deleted successfully'})
-        # Return error if todo not found
-        return jsonify({'message': 'Todo not found'}), 404
-    except Exception as e:
-        # Return error if something is wrong
-        return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
+    # Delete the todo based on ID
+    for i, todo in enumerate(todos):
+        if todo['id'] == todo_id:
+            todos.pop(i)
+            return jsonify({'message': 'Todo deleted successfully'})
+    # Return error if todo not found
+    abort(404,description='Todo not found')
 
 if __name__ == '__main__':
     # Run the flask app
